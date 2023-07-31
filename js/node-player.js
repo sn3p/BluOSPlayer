d2494c7cf651b2e97f7a950f99fffb861f493d5a
Matthijs Kuiper <info@matthijskuiper.nl>
2023-07-31 13:59:36 +0200

    longpolling

--- js/node-player.js
+++ js/node-player.js
@@ -4,6 +4,10 @@
  * See Node API docs for more info:
  * https://content-bluesound-com.s3.amazonaws.com/uploads/2022/07/BluOS-Custom-Integration-API-v1.5.pdf
  *
+ * TODO:
+ * - API class for device commands
+ * - Player class for player commands (with getters/setters)
+ *
  * @constructor
  * @param {HTMLElement} element - The player element.
  * @param {string} host - The host or IP address of the Node.
@@ -17,9 +21,8 @@ class NodePlayer {
     this.setupUI();
     this.setupControls();
 
-    // Get player status
-    // this.status({ timeout: 100, etag: "123" });
-    this.status();
+    // Start longpolling
+    this.longpoll();
   }
 
   setupUI() {
@@ -63,43 +66,75 @@ class NodePlayer {
     this.controls.volume = volume;
   }
 
-  // TODO:
-  // - Handle timeout and etag
-  // - Get diff of changes
+  // Start listening to the player
+  // TODO: Check for SyncStatus?
+  async longpoll() {
+    const xml = await this.status({ timeout: 100, etag: this.etag });
+
+    // Set etag for next request (longpolling)
+    const etag = xml.querySelector("status").getAttribute("etag");
+    // console.debug(etag, this.etag === etag ? "(not changed)" : "(changed)");
+    this.etag = etag;
+
+    // Update player
+    this.updatePlayer(xml);
+
+    // start longpoll again
+    // TODO: Handle timeout/errors?
+    // DOCS: When long-polling is being used then a client must not make two consecutive
+    //  requests for the same resource less than one second apart, even if the first
+    //  request returns in less than one second.
+    this.longpoll();
+  }
+
   async status(params = {}) {
+    // Build query
     const urlParams = new URLSearchParams(params);
     const query = `/Status?${urlParams.toString()}`;
 
+    // Get XML
     const xml = await this.query(query);
-
-    const doc = this.parseXML(xml);
-    const statusNode = doc.querySelector("status");
-
-    // console.debug("etag:", statusNode.getAttribute("etag"));
-
-    // Set track info
-    const title = this.statusValue(doc, "title1");
-    const artist = this.statusValue(doc, "title2");
-    const album = this.statusValue(doc, "title3");
-    this.setInfo(title, artist, album);
-
-    // Set album art
-    const image = this.statusValue(doc, "image");
-    this.setAlbumArt(image);
-
-    // Set playback state
-    const secs = this.statusValue(doc, "secs");
-    const totlen = this.statusValue(doc, "totlen");
-    this.setPlaybackState(secs, totlen);
-
-    // Set volume
-    this.controls.volume.disabled = false;
-    this.controls.volume.value = statusNode.querySelector("volume").textContent;
+    return this.parseXML(xml);
   }
 
-  // TODO: Multiple values at once
-  statusValue(doc, name) {
-    return doc.getElementsByTagName(name)[0]?.textContent;
+  // TODO: skip update if etag hasn't changed?
+  updatePlayer(xml) {
+    // Set track info
+    const title = this.statusValue(xml, "title1");
+    if (title) {
+      const artist = this.statusValue(xml, "title2");
+      const album = this.statusValue(xml, "title3");
+      this.setInfo(title, artist, album);
+    }
+
+    // Set album art
+    const image = this.statusValue(xml, "image");
+    if (image) {
+      this.setAlbumArt(image);
+    }
+
+    // Set playback state
+    // DOCS: Clients are required to increment the playback position, when
+    //  state is play or stream, based on the interval since the response.
+    const secs = this.statusValue(xml, "secs");
+    const totlen = this.statusValue(xml, "totlen");
+    if (secs && totlen) {
+      this.setPlaybackState(secs, totlen);
+    }
+
+    // Set volume
+    const volume = this.statusValue(xml, "volume");
+    if (volume) {
+      this.controls.volume.disabled = false;
+      this.controls.volume.value = volume;
+    } else {
+      this.controls.volume.disabled = true;
+    }
+  }
+
+  // TODO: Multiple values at once?
+  statusValue(xml, name) {
+    return xml.querySelector(name)?.textContent;
   }
 
   setInfo(title, artist, album) {
